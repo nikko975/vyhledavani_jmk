2cfd8a210bbe7ff6259764df42ada093f29e366f
Michal Cichra <michal@cichra.cz>
2018-04-23 20:05:54 +0200

    ability to start in daemon mode

--- server.rb
+++ server.rb
@@ -1,7 +1,24 @@
-# freeze_string_literals: true
+# frozen_string_literal: true
 
 require 'webrick'
 require_relative 'nahled'
+
+require 'optparse'
+
+options = { daemon: false }
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$PROGRAM_NAME} [options]"
+
+  opts.on('-d', '--daemon', 'Run as daemon') do |v|
+    options[:daemon] = v
+  end
+
+  opts.on('-h', '--help', 'Prints this help') do
+    puts opts
+    exit
+  end
+end.parse!
 
 root = File.expand_path './public_html'
 server = WEBrick::HTTPServer.new Port: ENV.fetch('PORT') { 8000 }, DocumentRoot: root
@@ -10,4 +27,6 @@ trap 'INT' do server.shutdown end
 
 server.mount '/search', Nahled
 
-server.start
+start = server.method(:start)
+
+options[:daemon] ? WEBrick::Daemon.start(&start) : start.call
